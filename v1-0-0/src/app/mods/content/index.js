3813fd33f15125adb4b9e0ca63d0228253ead34e
JamilServices <jamilservicos@gmail.com>
2026-01-18 00:23:44 -0300

    :sparkles: feat(struct): add header

--- v1-0-0/src/app/mods/content/index.js
+++ v1-0-0/src/app/mods/content/index.js
@@ -3,8 +3,19 @@
 import {CopyRightContent} from "@components/content/copyright.js";
 //import {MaintenanceContent} from "@components/content/maintenance.js";
 import {PageContent} from "@components/content/page/index.js";
+import {HeaderContent} from "@components/content/header/index.js";
 
 let Components;
+
+const headerContent = () => {
+    const parent = Components.container.store.element.recover("header.canvas");
+
+    if(!parent) return;
+    const element = HeaderContent(Components.container);
+    if(!element) return;
+
+    parent.appendChild(element);
+};
 
 const mainContent = () => {
     const parent = Components.container.store.element.recover("main.canvas");
@@ -24,6 +35,7 @@ const modInterface = Object.create(null);
 modInterface.page = PageContent;
 modInterface.start_request = (context = {}) => {
     Components = context.Components;
+    headerContent();
     mainContent();
     CopyRightContent(footerContent);
 };

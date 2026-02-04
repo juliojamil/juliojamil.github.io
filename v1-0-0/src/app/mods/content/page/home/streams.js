5290255ae8b4af1481b706d395f80b66b6d4ce9b
JamilServices <jamilservicos@gmail.com>
2026-02-04 02:01:12 -0300

    :wrench: refactor(main): add dinamic content

--- v1-0-0/src/app/mods/content/page/home/streams.js
+++ v1-0-0/src/app/mods/content/page/home/streams.js
@@ -2,25 +2,10 @@
 "use strict";
 
 const modState = {
-    items: {
-        twitch: {
-            name: "twitch",
-            link: "https://www.twitch.tv/juliojamil",
-            desc: "desenvolvimento web e desktop, e casualmente fa\u00E7o algumas gameplays."
-        },
-        trovo: {
-            name: "trovo",
-            link: "https://trovo.live/juliojamil",
-            desc: "minigame da trovo com intera\u00E7\u00E3o entre contas, com gameplay de desktop game, e casualmente vetoriza\u00E7\u00E3o de imagens."
-        },
-        kick: {
-            name: "kick",
-            link: "https://kick.com/juliojamil",
-            desc: "ainda n\u00E3o est\u00E1 definido ent\u00E3o fa\u00E7o lives diversas casualmente."
-        }
-    }
+    source: "/assets/streams.json",
+    items: null
 };
-Object.freeze(modState);
+Object.seal(modState);
 
 const line_build = (container, stream) => {
     if(stream && !modState.items[stream]) return undefined;
@@ -50,20 +35,62 @@ const line_build = (container, stream) => {
     return line;
 };
 
+const items_mount = (context = {}) => {
+    const {container, section} = context;
+    const {items} = modState;
+
+    if(!items) return;
+    const keys = Object.keys(modState.items);
+    const len = keys.length;
+    if(len === 0) return;
+
+    for(let i = 0; len > i; i++) {
+        const key = keys[i];
+        const item = items[key];
+        if(!item) continue;
+        const {name} = item;
+        if(!name) continue;
+        const line = line_build(container, name);
+        if(line) section.appendChild(line);
+
+    }
+};
+
+const extract_streams = (body, context) => {
+    body.json()
+        .then(data => {
+        if(!data?.items) return;
+        modState.items = data.items;
+        Object.freeze(modState.items);
+        items_mount(context);
+    });
+};
+
+const get_streams = (context = {}) => {
+    try {
+        fetch("/assets/streams.json")
+            .then(body => {
+                if(body.ok) extract_streams(body, context);
+            });
+    } catch (err) {
+        console.error(err.message);
+    }
+};
+
 const modInterface = Object.create(null);
 modInterface.content = (container) => {
     if(!container) return undefined;
     const section = container.element.listUl();
     section.setAttribute("class", "streams-list");
 
-    const twitch_line = line_build(container, "twitch");
+    /*const twitch_line = line_build(container, "twitch");
     const trovo_line = line_build(container, "trovo");
     const kick_line = line_build(container, "kick");
 
     if(twitch_line) section.appendChild(twitch_line);
     if(trovo_line) section.appendChild(trovo_line);
-    if(kick_line) section.appendChild(kick_line);
-
+    if(kick_line) section.appendChild(kick_line);*/
+    if(!modState.items) get_streams({container, section});
     return section;
 };
 Object.freeze(modInterface);
